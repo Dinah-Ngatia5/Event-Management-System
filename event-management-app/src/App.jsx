00f13479c07c571ed69cbf18ba972f50ced220f1
oriri04 <152679239+oriri04@users.noreply.github.com>
2024-06-20 21:35:05 +0300

    Signup-form

--- event-management-app/src/App.jsx
+++ event-management-app/src/App.jsx
@@ -1,14 +1,17 @@
-
-import './App.css'
+// src/App.js
+import React from 'react';
+import SignupForm from './components/SignupForm';
+import './App.css';
 
 function App() {
- 
-
   return (
-    <>
-     <h1>Event management app</h1> 
-    </>
-  )
+    <div className="App">
+      <header className="App-header">
+        <h1>Management App</h1>
+        <SignupForm />
+      </header>
+    </div>
+  );
 }
 
-export default App
+export default App;
